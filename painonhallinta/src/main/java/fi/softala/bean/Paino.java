ae014e25c1d7fd7ccd5fa27709c7653606b12569
jusju <jukka.juslin@haaga-helia.fi>
2016-01-04 20:01:39 +0200

    setterit

--- painonhallinta/src/main/java/fi/softala/bean/Paino.java
+++ painonhallinta/src/main/java/fi/softala/bean/Paino.java
@@ -2,4 +2,13 @@ package fi.softala.bean;
 
 public class Paino {
 	private double paino;
+
+	public double getPaino() {
+		return paino;
+	}
+
+	public void setPaino(double paino) {
+		this.paino = paino;
+	}
+	
 }
